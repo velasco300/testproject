b071cedce2c1114644e1e147fbaffdfc69ce1ddd
Your Name <you@example.com>
2016-08-31 00:13:14 -0700

    dddd

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,3 +38,15 @@ set :repo_url, 'https://github.com/velasco300/testproject.git'
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
+
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,   "#{shared_path}/tmp/pids/puma.pid"
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :db
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 6]
+set :puma_workers, 0
+set :puma_init_active_record, false
+set :puma_preload_app, true
