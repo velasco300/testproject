5a800f0891e7c73d5ededcfdf7c3ae605080f99d
Your Name <you@example.com>
2016-08-30 23:13:07 -0700

    fdfdf

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,3 +34,9 @@ set :repo_url, 'https://github.com/velasco300/testproject.git'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
