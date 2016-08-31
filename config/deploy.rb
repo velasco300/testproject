239c11f68d89613c44813b574d69c878a2c3e316
Your Name <you@example.com>
2016-08-30 23:22:00 -0700

    aaaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,5 +38,3 @@ set :repo_url, 'https://github.com/velasco300/testproject.git'
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
