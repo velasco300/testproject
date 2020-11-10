10f684fca44bbe3ed05f78753db4242372406c66
wuweihong <weihong@timesdata.cn>
2020-11-10 20:40:32 +0800

    feat(service): the two example
    1.aaaaa
    2.bbbbbb

--- src/main/java/com/zzz/testproject/TestprojectApplication.java
+++ src/main/java/com/zzz/testproject/TestprojectApplication.java
@@ -9,6 +9,7 @@ public class TestprojectApplication {
 	public static void main(String[] args) {
 		System.out.println("----------------");
 		SpringApplication.run(TestprojectApplication.class, args);
+		System.out.println("=============");
 	}
 
 }
